7ed05021fd90828df9a74aac302bcfed22ae3650
Alexa Jauregui <alexa7carol@gmail.com>
2026-03-09 20:26:50 -0700

    Added stored procedures to PL.sql to implement CUD for Players

--- PL.sql
+++ PL.sql
@@ -12,3 +12,43 @@ BEGIN
     DELETE FROM Players WHERE username = 'ProGamer99';
 END //
 DELIMITER ;
+
+-- CREATE player sp
+DROP PROCEDURE IF EXISTS sp_insert_player;
+DELIMITER //
+CREATE PROCEDURE sp_insert_player(
+    IN p_username VARCHAR(100),
+    IN p_email VARCHAR(255)
+)
+BEGIN
+    INSERT INTO Players (username, email)
+    VALUES (p_username, p_email);
+END //
+DELIMITER ;
+
+-- UPDATE player sp
+DROP PROCEDURE IF EXISTS sp_update_player;
+DELIMITER //
+CREATE PROCEDURE sp_update_player(
+    IN p_playerID INT,
+    IN p_username VARCHAR(100),
+    IN p_email VARCHAR(255)
+)
+BEGIN
+    UPDATE Players
+    SET username = p_username, email = p_email
+    WHERE playerID = p_playerID;
+END //
+DELIMITER ;
+
+-- DELETE player sp
+DROP PROCEDURE IF EXISTS sp_delete_player;
+DELIMITER //
+CREATE PROCEDURE sp_delete_player(
+    IN p_playerID INT
+)
+BEGIN
+    DELETE FROM Players 
+    WHERE playerID = p_playerID;
+END //
+DELIMITER ;
